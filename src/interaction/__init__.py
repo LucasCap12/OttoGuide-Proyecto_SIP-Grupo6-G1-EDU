5ad1d345efad6d7cac45dfb22537accc9709971c
LucasCap12 <LucasCap12@users.noreply.github.com>
2026-03-30 20:37:53 -0300

    feat(nlp): pipeline conversacional hibrido con hot-swap local/cloud y sumidero ALSA

---
+++ src/interaction/__init__.py
@@ -0,0 +1,17 @@
+from .conversation_manager import (
+    CloudNLPPipeline,
+    ConversationManager,
+    ConversationRequest,
+    ConversationResponse,
+    LocalNLPPipeline,
+    NLPStrategy,
+)
+
+__all__ = [
+    "CloudNLPPipeline",
+    "ConversationManager",
+    "ConversationRequest",
+    "ConversationResponse",
+    "LocalNLPPipeline",
+    "NLPStrategy",
+]
